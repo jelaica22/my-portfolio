d59bc24c300cbecf02af4b3f42e42f8ec9d04f91
jelaica22 <jelaicabtabanag@gmail.com>
2025-04-18 15:58:42 +0800

    Collected static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
+# settings.py
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # For dev
+STATIC_ROOT = BASE_DIR / "staticfiles"    # For collectstatic (prod)
 
 
 # Default primary key field type
